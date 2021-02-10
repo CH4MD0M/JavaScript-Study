aee529551a2128b29d427727210f7d3d6b96d67e
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-02-10 22:19:15 +0900

    ES6 Classes

---
+++ OOP/class.js
@@ -0,0 +1,65 @@
+'console.log(`Hey ${this.firstName}`);use strict';
+// class 표현식
+// const PersonCl = class{}
+
+// class 선언문
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Class는 호이스트 되지 않는다.
+// 2. Class는 일급 객체이다.
+// 3. Class는 strict mode(엄격한 모드)에서 실행된다.
+
+const account = {
+  owner: 'domm',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
